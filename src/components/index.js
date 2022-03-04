110685598962558d592f6686781fae14be2736a9
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-03-04 14:38:13 -0500

    Add import components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Button from './atoms/Button/Button';
+import CheckBox from './atoms/CheckBox/CheckBox';
+import TextInput from './atoms/Input/Input';
+import Spinner from './atoms/Spinner/Spinner';
+
+export { Button, CheckBox, TextInput, Spinner };
